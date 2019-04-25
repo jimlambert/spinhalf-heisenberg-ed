9d4f5e61aaaaffb03d742d786bedbfe49ac3bb65
castled-rook <james200814@hotmail.com>
2019-04-25 13:14:47 -0400

    Just finding eigenvalues

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ include("EDutils.jl")
 
 using LinearAlgebra
 
-N = 2
+N = 4
 q = pi
 β = 10
 tmin = 0
@@ -21,48 +21,47 @@ sx2vals = []
 # determine eigenvalues/vectors of H
 F = eigen(genH)
 U = F.vectors
-gstate = U[:,1]
-
-println("exact diagonalization complete")
-
-
-# generate sz operators
-for i::Int=1:(N/2)
-  push!(szarr, inv(U)*0.5*EDutils.initσz(N, i)*U)
-  push!(sxarr, inv(U)*0.5*EDutils.initσx(N, i)*U)
-end
-
-sztot = zeros(2^N, 2^N)
-println(szarr)
-for (r,sz) ∈ enumerate(szarr)
-  global sztot += exp(1im*q*r)*sz
-end
-sztot = sztot / N
-sztot2 = sztot*sztot
-
-
-sxtot = zeros(2^N, 2^N)
-for (r,sx) ∈ enumerate(sxarr)
-  global sxtot += exp(1im*q*r)*sx
-end
-sxtot = sxtot / N
-sxtot2=sxtot*sxtot
-
-for t in tarr 
-  global amplitude
-  global sztot2
-  #global sxtot2
-  global sztot
-  #global sxtot
-  gstatenew = exp(-1im*t*evoH)*gstate 
-  push!(amplitude, abs(adjoint(gstate)*gstatenew))
-  global gstate = gstatenew
-  #push!(sztot2, adjoint(gstate)*sztot2*gstate)
-  #push!(sxtot2, adjoint(gstate)*sxtot2*gstate)
-  #push!(sztot, adjoint(gstate)*sztot*gstate)
-  #push!(sxtot, adjoint(gstate)*sxtot*gstate)
-end
-println(amplitude)
+println(F.values)
+#gstate = (1.0/(2^(0.5)))*(U[:,1]-U[:,4])
+#
+#println("exact diagonalization complete")
+#
+#
+## generate sz operators
+#for i::Int=1:(N/2)
+#  push!(szarr, inv(U)*0.5*EDutils.initσz(N, i)*U)
+#  push!(sxarr, inv(U)*0.5*EDutils.initσx(N, i)*U)
+#end
+#
+#sztot = zeros(2^N, 2^N)
+#for (r,sz) ∈ enumerate(szarr)
+#  global sztot += exp(1im*q*r)*sz
+#end
+#sztot = sztot / N
+#sztot2 = sztot*sztot
+#
+#
+#sxtot = zeros(2^N, 2^N)
+#for (r,sx) ∈ enumerate(sxarr)
+#  global sxtot += exp(1im*q*r)*sx
+#end
+#sxtot = sxtot / N
+#sxtot2=sxtot*sxtot
+#for t in tarr 
+#  global amplitude
+#  global sztot2
+#  #global sxtot2
+#  global sztot
+#  #global sxtot
+#  gstatenew = exp(-1im*t*evoH)*gstate 
+#  push!(amplitude, abs(adjoint(gstate)*gstatenew))
+#  push!(sz2vals, adjoint(gstate)*sztot2*gstate)
+#  push!(sx2vals, adjoint(gstate)*sxtot2*gstate)
+#  push!(szvals, adjoint(gstate)*sztot*gstate)
+#  push!(sxvals, adjoint(gstate)*sxtot*gstate)
+#end
+#println(amplitude)
+#println(szvals)
 # sz_0 = szarr[1]
 # sz_τexpvals = []
 # sz_texpvals = []
